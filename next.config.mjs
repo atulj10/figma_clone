aeda2aa8a6365745dafd91af813b25ce82caffca
atulj10 <atulanandj10@gmail.com>
2024-06-19 10:50:45 +0530

    upload build with a little issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+    webpack: (config) => {
+        config.externals.push({
+            'utf-8-validate': "commonjs utf-8-validate",
+            'bufferutil': 'commonjs bufferutil',
+            canvas: 'commonjs canvas',
+        })
+
+        return config;
+    },
+    images: {
         remotePatterns: [
-        {
-            protocol:"https",
-            hostname:"liveblocks.io",
-            port:""
-        }
-    ]}
+            {
+                protocol: "https",
+                hostname: "liveblocks.io",
+                port: ""
+            }
+        ]
+    },
+
+    typescript: {
+        ignoreBuildErrors: true,
+    }
 };
 
 export default nextConfig;
